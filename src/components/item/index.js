928545f735d669ff4bbeb2e6dbdc5b4ca9d11bd8
David <larin.d1805@gmail.com>
2024-09-17 00:35:04 +0300

    use default args instead of defaultProps

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { plural } from '../../utils';
 import './style.css';
 
-function Item(props) {
+function Item({ onDelete = () => {}, onSelect = () => {}, ...props }) {
   // Счётчик выделений
   const [count, setCount] = useState(0);
 
   const callbacks = {
     onClick: () => {
-      props.onSelect(props.item.code);
+      onSelect(props.item.code);
       if (!props.item.selected) {
         setCount(count + 1);
       }
     },
     onDelete: e => {
       e.stopPropagation();
-      props.onDelete(props.item.code);
+      onDelete(props.item.code);
     },
   };
 
@@ -52,11 +52,6 @@ Item.propTypes = {
   }).isRequired,
   onDelete: PropTypes.func,
   onSelect: PropTypes.func,
-};
-
-Item.defaultProps = {
-  onDelete: () => {},
-  onSelect: () => {},
 };
 
 export default React.memo(Item);
