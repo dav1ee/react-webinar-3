e3c88c78b2936ee35b2b944a93d53164f780cf68
David <larin.d1805@gmail.com>
2024-09-25 10:39:13 +0300

    refactor layout

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,49 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, matchPath } from 'react-router-dom';
+
 import Basket from './basket';
+
+import PageLayout from '../components/page-layout';
+import Head from '../components/head';
+import BasketTool from '../components/basket-tool';
+
 import useStore from '../store/use-store';
 import useSelector from '../store/use-selector';
+
+import { ROUTER_PATHS } from '../constants';
 
 /**
  * Приложение
  * @returns {React.ReactElement}
  */
 function App() {
-  const activeModal = useSelector(state => state.modals.name);
+  const location = useLocation();
+  const store = useStore();
+
+  const select = useSelector(state => ({
+    productTitle: state.productDetails.item?.title,
+    amount: state.basket.amount,
+    sum: state.basket.sum,
+    activeModal: state.modals.name,
+  }));
+
+  const callbacks = {
+    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+  };
+
+  const title =
+    matchPath(ROUTER_PATHS.PRODUCT_DETAILS, location.pathname) && !!select.productTitle
+      ? select.productTitle
+      : 'Магазин';
 
   return (
     <>
-      <Outlet />
-      {activeModal === 'basket' && <Basket />}
+      <PageLayout head={<Head title={title} />}>
+        <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} />
+        <Outlet />
+      </PageLayout>
+
+      {select.activeModal === 'basket' && <Basket />}
     </>
   );
 }
--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,30 +1,17 @@
 import { memo, useCallback, useEffect } from 'react';
+
 import Item from '../../components/item';
-import PageLayout from '../../components/page-layout';
-import Head from '../../components/head';
-import BasketTool from '../../components/basket-tool';
 import List from '../../components/list';
+
 import useStore from '../../store/use-store';
 import useSelector from '../../store/use-selector';
 
 function Main() {
   const store = useStore();
-
-  useEffect(() => {
-    store.actions.catalog.load();
-  }, []);
-
-  const select = useSelector(state => ({
-    list: state.catalog.list,
-    amount: state.basket.amount,
-    sum: state.basket.sum,
-  }));
+  const list = useSelector(state => state.catalog.list);
 
   const callbacks = {
-    // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
-    // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
   };
 
   const renders = {
@@ -36,13 +23,11 @@ function Main() {
     ),
   };
 
-  return (
-    <PageLayout>
-      <Head title="Магазин" />
-      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} />
-      <List list={select.list} renderItem={renders.item} />
-    </PageLayout>
-  );
+  useEffect(() => {
+    store.actions.catalog.load();
+  }, []);
+
+  return <List list={list} renderItem={renders.item} />;
 }
 
 export default memo(Main);
